b8817d5d34dda31aada5345ff1e5b28f6d0415f4
brtyvz <beratttyavuzz@gmail.com>
2021-12-12 14:38:13 +0300

    commit

--- BildirimDeneme/ViewController.swift
+++ BildirimDeneme/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yazdırLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func bildirim(_ sender: Any) {
+       //butona tıkladığımızda alert oluşturacak
+        let alertController = UIAlertController(title: "Başlık", message: "kaydet", preferredStyle: .alert)
+        // oluşturulan alerte şifre ve mail textfieldi ekler
+        alertController.addTextField{
+            textfield in
+            textfield.placeholder = "Mail"
+            textfield.keyboardType = .emailAddress
+        }
+        alertController.addTextField{
+            textfield in
+            textfield.placeholder = "Şifre"
+            textfield.isSecureTextEntry = true
+        }
+        let kaydetAction = UIAlertAction(title: "Kaydet", style: .destructive) { action in
+            let alinanEmail = (alertController.textFields![0] as UITextField).text!
+            let alinanSifre = (alertController.textFields![1] as UITextField).text!
+            self.yazdırLabel.text = "mail:\(alinanEmail)-sifre:\(alinanSifre)"
+        }
+        //tektler görülsün diye
+        alertController.addAction(kaydetAction)
+        self.present(alertController, animated: true)
+    }
+    
+    @IBAction func kaydet(_ sender: Any) {
+    }
+    
+    
 }
 
